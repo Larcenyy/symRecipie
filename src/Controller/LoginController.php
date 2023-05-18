35c331b43bd954da19b3fed8bf311fba75e6ace5
remyc <remyc59440@hotmail.com>
2023-05-18 11:26:18 +0200

    v0.1 18/05

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-class HomeController extends AbstractController
+class LoginController extends AbstractController
 {
-    #[Route('/', 'home.index', methods: ['GET'])]
+    #[Route('/connect', 'login.index', methods: ['GET'])]
+
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('connect/login.html.twig');
     }
+
 }
